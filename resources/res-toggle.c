18752bd0be390262c238f87b6d11bf4eac2aeeca
Kakrial <kristian.fiebig@gmail.com>
2020-10-14 11:44:37 +1100

    fds

--- resources/res-toggle.c
+++ resources/res-toggle.c
@@ -67,7 +67,8 @@ RESOURCE(res_toggle,
 static void
 res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
-  red_state = ~red_state;
+  red_state = 0;
+	green_state = 0;
 }
 
 //Event timer thread
